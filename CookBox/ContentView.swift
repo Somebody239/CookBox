c7ef21b095ccc973338d4152b333f71e373a932a
Kishan Joshi <Kishan.cut@gmail.com>
2026-07-12 23:21:55 -0400

    Initial Commit

---
+++ CookBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookBox
+//
+//  Created by Kishan on 2026-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookBox/CookBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBoxApp.swift
+//  CookBox
+//
+//  Created by Kishan on 2026-07-12.
+//
+
+import SwiftUI
+
+@main
+struct CookBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
