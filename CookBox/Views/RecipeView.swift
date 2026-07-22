f9c51fa196139654cf5646c71ddc1e4285179b23
Kishan Joshi <Kishan.cut@gmail.com>
2026-07-22 17:04:42 -0400

    Add recipe and recipe data updates

---
+++ CookBox/Views/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  CookBox
+//
+//  Created by Kishan on 2026-07-22.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
